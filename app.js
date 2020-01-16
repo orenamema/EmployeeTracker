e73857f29e129b2515541e90288ef07c2c447fbe
oren amema <orenamema@gmail.com>
2020-01-16 07:24:19 -0800

    adding function to add role

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var connection = mysql.createConnection({
   password: "password",
   database: "trackerdb"
 });
+
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
@@ -40,7 +41,7 @@ connection.connect(function (err) {
           message: "What would you like to do?",
           name: "choice",
           choices: ["add departments","add roles", "add employees ",
-          "view departments", "view roles", "view employees", "update employees" ]
+          "view departments", "view roles", "view employees", "update employees","all done?" ]
   
         }
       ]).then(function (res) {
@@ -66,5 +67,62 @@ connection.connect(function (err) {
         else if (res.choice ==="update employees"){
             updateEmployee();
         }
+        else {
+            console.log("Thank you! Come again!");
+        }
+    })
+}
+
+function addDpt () {
+    inquirer.prompt([
+    {
+        type:"input",
+        name: "name",
+        message: "what is the department name?"
+    }
+
+    ]).then(function(res){
+        connection.query(`INSERT INTO department (name)
+                          VALUES ("${res.name}");`
+                        , function (error, results, fields) {
+            if (error) throw error;
+            console.log('Department added to DB');
+        });
+        start();
+    })
+}
+function addRole () {
+    inquirer.prompt([
+    {
+        type:"input",
+        name: "title",
+        message: "what is the title?"
+    },
+
+    {
+        type:"input",
+        name: "salary",
+        message: "what is the salary?"
+    },
+
+    {
+        type:"input",
+        name: "departmentid",
+        message: "what is the department id?"
+    }
+
+    ]).then(function(res){
+        connection.query(
+            "INSERT INTO role SET ?",
+            {
+              title: res.title,
+              salary: res.salary,
+              department_id: res.departmentid
+            },
+        function (error) {
+            if (error) throw error;
+            console.log('Role added to DB');
+        });
+        start();
     })
 }
