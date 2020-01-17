70a9b89e27c6093bf4642a0563e67a6405051648
oren amema <orenamema@gmail.com>
2020-01-16 23:11:42 -0800

    commiting working code

--- app.js
+++ app.js
@@ -17,21 +17,11 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
+    console.log("connected as id " + connection.threadId + "\n");  
     start();
-  
-  });
+});
 
-//   function addDpt () {
-//     console.log("Inserting a new department..\n");
-//     var query = connection.query(
-//       "INSERT INTO department SET ?",
-//       function (err, res) {
-//         console.log(res.affectedRows + "department inserted!\n");
-//       }
-//     );
-//     console.log(query.sql);
-//   }
+
 
   function start() {
     inquirer
@@ -41,7 +31,7 @@ connection.connect(function (err) {
           message: "What would you like to do?",
           name: "choice",
           choices: ["add departments","add roles", "add employees",
-          "view departments", "view roles", "view employees", "update employees","all done?" ]
+          "view employees by departments", "view employees by roles", "view employees", "update employees","all done?" ]
   
         }
       ]).then(function (res) {
@@ -55,19 +45,20 @@ connection.connect(function (err) {
         else if (res.choice === "add employees"){
             addEmployee();
         }
-        else if (res.choice ==="view departments"){
+        else if (res.choice ==="view employees by departments"){
             viewDepartment();
         }
-        else if (res.choice ==="view roles"){
+        else if (res.choice ==="view employees by roles"){
             viewRole();
         }
         else if (res.choice ==="view employees"){
             viewEmployee();
         }
         else if (res.choice ==="update employees"){
-            updateEmployee();
+            updateEmployeeRole();
         }
         else { //we have to close the connection to close the program
+            connection.end();
             console.log("Thank you! Come again!");
         }
     })
@@ -91,11 +82,11 @@ function addDpt () {
             if (error) throw error;
 
             console.log('Department added to DB');
+            start();
         });
-        start();
     })
 }
-function addRole () {
+function addRole() {
     inquirer.prompt([
     {
         type:"input",
@@ -126,8 +117,9 @@ function addRole () {
         function (error) {
             if (error) throw error;
             console.log('Role added to DB');
+            start();
         });
-        start();
+
     })
 }
 function addEmployee () {
@@ -167,51 +159,162 @@ function addEmployee () {
         function (error) {
             if (error) throw error;
             console.log('Employee added to DB');
+            start();
         });
-        start();
     })
 }
 
 //View Functions
 
 function viewDepartment (){
-    connection.query( "SELECT * FROM department", function(err,res){
-        console.log(res);
-        if (err) throw err; 
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "department",
+                message: "Select departement",
+                choices: function () {
+                    var the_choices = []
+                    for (var i = 0; i < res.length; i++) {
+                        the_choices.push(res[i].name)
+                    }
+                    return the_choices;
+                }
+            }
+        ]).then(function (ans) {
+            connection.query(`
+                select e.*
+                from department d 
+                inner join role r
+                on d.id = r.department_id
+                inner join employee e
+                on e.role_id = r.id
+                where d.name = "${ans.department}"
+                `, function (err, res) {
+                    if (err) throw err;
+                    console.log(res);
+                    start();
+                });
+        });
     });
 }
 
 function viewRole (){
-    connection.query( "SELECT * FROM role", function(err,res){
-        console.log(res);
-        if (err) throw err; 
+    connection.query("SELECT * FROM role", function (err, res) {
+        if (err) throw err;
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "role",
+                message: "Select role",
+                choices: function () {
+                    var the_choices = []
+                    for (var i = 0; i < res.length; i++) {
+                        the_choices.push(res[i].title)
+                    }
+                    return the_choices;
+                }
+            }
+        ]).then(function (ans) {
+            connection.query(`
+                select e.*
+                from role r
+                inner join employee e
+                on e.role_id = r.id
+                where r.title = "${ans.role}"
+                `, function (err, res) {
+                    if (err) throw err;
+                    console.log(res);
+                    start();
+                });
+        });
     });
 }
 
 function viewEmployee (){
-    connection.query( "SELECT * FROM employee", function(err,res){
+    query = `select
+            e.id id, e.first_name first_name,
+            e.last_name last_name, r.title title,
+            d.name department, r.salary salary,
+            CONCAT(e2.first_name, " ", e2.last_name) manager
+            from employee e
+            left join role r
+            on e.role_id = r.id
+            left join department d
+            on r.department_id = d.id
+            left join employee e2
+            on e.manager_id = e2.id`
+    connection.query( query, function(err,res){
         console.log(res);
         if (err) throw err; 
-    });
-    start();
+        start();
+    });  
 }
 
-//Update Functions
+function viewData(query){
+    connection.query( query, function(err,res){
+        console.log(res);
+        if (err) throw err; 
+    });    
+}
 
-// function updateEmployee(){
-//     var query = connection.query()
-//     [
-//         {
-//           role_id: answer.role
-//         },
-//         {
-//           id: 
-//         }
-//       ],
-//       function(error) {
-//         if (error) throw err;
-//         console.log("Bid placed successfully!");
-//         start();
-//       }
-//     );
-//   }
+update_cust_id = 0;
+update_employees = {};
+update_roles = {};
+
+function updateEmployeeRole (){
+    connection.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
+        for (var i = 0; i < res.length; i++) {
+            update_employees[`${res[i].first_name} ${res[i].last_name}`] = res[i].id;
+        }
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "employee",
+                message: "Select employee",
+                choices: function () {
+                    var the_choices = []
+                    for (var i = 0; i < res.length; i++) {
+                        the_choices.push(`${res[i].first_name} ${res[i].last_name}`)
+                    }
+                    return the_choices;
+                }
+            }
+        ]).then(function (ans) {
+            update_cust_id = update_employees[ans.employee];
+            connection.query("SELECT * FROM role", function (err, res) {
+                if (err) throw err;
+                for (var i = 0; i < res.length; i++) {
+                    update_roles[res[i].title] = res[i].id;
+                }
+                inquirer.prompt([
+                    {
+                        type: "list",
+                        name: "role",
+                        message: "Select new role",
+                        choices: function () {
+                            var the_choices = []
+                            for (var i = 0; i < res.length; i++) {
+                                the_choices.push(res[i].title)
+                            }
+                            return the_choices;
+                        }
+                    }
+                ]).then(function (ans) {
+                    connection.query(`
+                    UPDATE employee
+                    SET role_id = ${update_roles[ans.role]}
+                    WHERE id = ${update_cust_id};
+                        `, function (err, res) {
+                            if (err) throw err;
+                            console.log(`Role updated!`);
+                            start();
+                    });
+                });
+            });
+        });
+    });
+
+}
